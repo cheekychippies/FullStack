fb5f61719a9f3150863123a91ecd5f940b25caea
cheekychippies <104887666+cheekychippies@users.noreply.github.com>
2025-04-07 09:44:49 +1200

    keskiarvo ja prosentti toimii

--- osa1/unicafe/part1/src/App.jsx
+++ osa1/unicafe/part1/src/App.jsx
@@ -9,24 +9,44 @@ const Button = (props) => {
 }
 const Display = ({ counter }) => <div>All: {counter}</div>
 
+const Statistics = ({ counter, rating }) => {
+  const totalSum = rating.reduce((sum, value) => sum + value, 0)
+  const average = counter > 0 ? totalSum / counter : 0
+  const positive = rating.filter(value => value === 1).length
+  console.log(positive)
+  const persentage = counter > 0 ? positive / counter : 0 
+  return (
+    <div>
+      Average: {average.toFixed(2)} <br />
+      Persentage {persentage.toFixed(2)}
+    </div>
+  )
+}
+
+
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [counter, setCounter] = useState(0)
+  const [rating, setRating] = useState([])
 
   const handleGoodClick = () => {
     setGood(good + 1)
     setCounter(counter + 1)
+    setRating(rating.concat(1))
   }
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
     setCounter(counter + 1)
+    setRating(rating.concat(0))
   }
   const handleBadClick = () => {
     setBad(bad + 1)
     setCounter(counter + 1)
+    setRating(rating.concat(-1))
   }
 
   return (
@@ -40,6 +60,7 @@ const App = () => {
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
       <Display counter={counter} />
+      <Statistics counter={counter} rating={rating} />
 
     </div>
   )
