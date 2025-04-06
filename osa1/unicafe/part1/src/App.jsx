51f7c229eebe18f57ec1b960530b60af52715903
cheekychippies <104887666+cheekychippies@users.noreply.github.com>
2025-04-07 10:08:32 +1200

    statistics

--- osa1/unicafe/part1/src/App.jsx
+++ osa1/unicafe/part1/src/App.jsx
@@ -7,48 +7,61 @@ const Button = (props) => {
     <button onClick={onClick}>{text}</button>
   )
 }
-const Display = ({ counter }) => <div>All: {counter}</div>
+//const Display = ({ counter }) => <div>All: {counter}</div>
 
 const Statistics = ({ counter, rating }) => {
   const totalSum = rating.reduce((sum, value) => sum + value, 0)
   const average = counter > 0 ? totalSum / counter : 0
   const positive = rating.filter(value => value === 1).length
+  const neutral = rating.filter(value => value === 0).length
+  const bad = rating.filter(value => value === -1).length
   console.log(positive)
-  const persentage = counter > 0 ? positive / counter : 0 
+  const persentage = counter > 0 ? positive / counter : 0
+
+  if (counter > 0) {
+    return (
+      <div>
+        Good: {positive} <br />
+        Neutral: {neutral} <br />
+        Bad: {bad} <br />
+        All: {counter} <br />
+        Average: {average.toFixed(2)} <br />
+        Persentage {persentage.toFixed(2)}
+      </div>
+    )
+  }
   return (
-    <div>
-      Average: {average.toFixed(2)} <br />
-      Persentage {persentage.toFixed(2)}
-    </div>
+    <p>No Feedback given</p>
   )
+
 }
 
 
 
 const App = () => {
   // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  //const [good, setGood] = useState(0)
+  //const [neutral, setNeutral] = useState(0)
+  //const [bad, setBad] = useState(0)
   const [counter, setCounter] = useState(0)
   const [rating, setRating] = useState([])
 
   const handleGoodClick = () => {
-    setGood(good + 1)
+    //setGood(good + 1)
     setCounter(counter + 1)
     setRating(rating.concat(1))
   }
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
+    // setNeutral(neutral + 1)
     setCounter(counter + 1)
     setRating(rating.concat(0))
   }
   const handleBadClick = () => {
-    setBad(bad + 1)
+    //setBad(bad + 1)
     setCounter(counter + 1)
     setRating(rating.concat(-1))
   }
-
+  // <Display counter={counter} />
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -56,10 +69,6 @@ const App = () => {
       <Button onClick={handleNeutralClick} text='neutral' />
       <Button onClick={handleBadClick} text='bad' />
       <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <Display counter={counter} />
       <Statistics counter={counter} rating={rating} />
 
     </div>
