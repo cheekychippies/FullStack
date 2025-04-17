dcb9b1294ece772622187f0e98170b2d2b090b78
cheekychippies <104887666+cheekychippies@users.noreply.github.com>
2025-04-17 18:49:51 +1200

    2.11

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas', number: '040-123456' },
-    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
-    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
-    { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setNewFilterName] = useState('')
 
-  //console.log('tosi? ',)
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  })
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -59,7 +61,7 @@ const App = () => {
         newName={newName}
         newNumber={newNumber}
         handlePersonChange={handlePersonChange}
-        handleNumberChange={handleNumberChange}  
+        handleNumberChange={handleNumberChange}
       />
       <h2>Numbers</h2>
       <Persons
